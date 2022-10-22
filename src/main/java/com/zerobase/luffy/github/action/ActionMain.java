31aa53315408532f0b795f444dbbb5d039662d27
moon <moonlt93@naver.com>
2022-10-22 13:39:21 +0900

    commit 12

--- src/main/java/com/zerobase/luffy/github/action/ActionMain.java
+++ src/main/java/com/zerobase/luffy/github/action/ActionMain.java
@@ -2,6 +2,14 @@ package com.zerobase.luffy.github.action;
 
 public class ActionMain {
     public static void main(String[] args) {
-        System.out.println("실행중");
+
+        cal(1);
+
+    }
+
+    public static void cal(int num){
+        for (int i = num; i < 2; i++) {
+            System.out.println("메시지");
+        }
     }
 }

cc05372447dc40e5603e34a82804694340574b3d
moon <moonlt93@naver.com>
2022-10-22 14:05:48 +0900

    commit 15

---
+++ src/test/java/com/zerobase/luffy/member/common/repository/MemberRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.zerobase.luffy.member.common.repository;
+
+import com.zerobase.luffy.member.common.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void cal(){
+
+     int a = 23;
+     assertEquals(a,23);
+    }
+
+}

fe88bfa9b15602c75c18940582844fec73a648a9
moon <moonlt93@naver.com>
2022-10-22 19:46:07 +0900

    commit 16

--- src/test/java/com/zerobase/luffy/LuffyApplicationTest.java
+++ src/test/java/com/zerobase/luffy/LuffyApplicationTest.java
@@ -3,14 +3,18 @@ package com.zerobase.luffy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
 class LuffyApplicationTest {
 
     @Test
-    void contextLoads() {
-    }
+    public void comment(){
+        System.out.println("test");
+        int num =1;
 
+        assertEquals(1,num);
+
+    }
 }

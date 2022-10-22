c204f9de0449873460826f018ad17dad5e70c709
moon <moonlt93@naver.com>
2022-10-22 11:32:22 +0900

    commit 7

--- src/test/java/com/zerobase/luffy/LuffyApplicationTests.java
+++ src/test/java/com/zerobase/luffy/LuffyApplicationTests.java
@@ -3,12 +3,22 @@ package com.zerobase.luffy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.equalTo;
+
 @SpringBootTest
 class LuffyApplicationTests {
 
     @Test
-    void contextLoads() {
-
+    void sampleTest() {
+        assertThat(1, anything());
     }
+
+    @Test
+    void equalTest() {
+        assertThat(1, equalTo(1));
+    }
+
 
 }

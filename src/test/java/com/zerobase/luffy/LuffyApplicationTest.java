a3b4b43a365c881b30faa4c09d02a4db275716d8
moon <moonlt93@naver.com>
2022-10-22 22:38:51 +0900

    commit 20

--- src/test/java/com/zerobase/luffy/LuffyApplicationTest.java
+++ src/test/java/com/zerobase/luffy/LuffyApplicationTest.java
@@ -2,9 +2,11 @@ package com.zerobase.luffy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 
 @SpringBootTest
+@EnableSpringConfigured
 class LuffyApplicationTest {
     @Test
     void contextLoads() {

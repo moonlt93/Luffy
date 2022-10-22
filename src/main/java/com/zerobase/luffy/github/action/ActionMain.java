1796f3fe2a76792c7e2ac7a6fe9f49bd53a0da48
moon <moonlt93@naver.com>
2022-10-22 13:29:23 +0900

    commit 12

---
+++ src/main/java/com/zerobase/luffy/github/action/ActionMain.java
@@ -0,0 +1,7 @@
+package com.zerobase.luffy.github.action;
+
+public class ActionMain {
+    public static void main(String[] args) {
+        System.out.println("실행중");
+    }
+}
---
+++ src/test/java/com/zerobase/luffy/github/action/ActionMainTest.java
@@ -0,0 +1,15 @@
+package com.zerobase.luffy.github.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ActionMainTest {
+
+    @Test
+    void main() {
+        assertEquals(1,1);
+    }
+}

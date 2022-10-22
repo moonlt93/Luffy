6ff7c32dad5d9c305d4d67013e1d77785d6f882e
moon <moonlt93@naver.com>
2022-10-22 11:06:18 +0900

    commit 4

--- src/test/java/com/zerobase/luffy/LuffyApplicationTests.java
+++ src/test/java/com/zerobase/luffy/LuffyApplicationTests.java
@@ -1,13 +1,10 @@
 package com.zerobase.luffy;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LuffyApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
--- src/test/java/com/zerobase/luffy/TestapiTest.java
+++ src/test/java/com/zerobase/luffy/TestapiTest.java
@@ -1,7 +1,5 @@
 package com.zerobase.luffy;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,24 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 class TestapiTest {
 
 
-@Test
-void getfile(){
-//given
-Assertions.assertEquals(1, 1);
-//when
 
-//then
-
-}
-    @Test
-    void getfile2(){
-//given
-        Assertions.assertEquals(2, 2);
-//when
-
-//then
-
-    }
 
 
 }

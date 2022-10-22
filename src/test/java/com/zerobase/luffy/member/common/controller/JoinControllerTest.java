cdc75b2a6d33ca7b9974577af44b0428c628ece4
moon <moonlt93@naver.com>
2022-10-22 20:10:57 +0900

    commit 16

---
+++ src/test/java/com/zerobase/luffy/member/common/controller/JoinControllerTest.java
@@ -0,0 +1,27 @@
+package com.zerobase.luffy.member.common.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RequiredArgsConstructor
+@Transactional
+class JoinControllerTest {
+
+
+    @Test
+    public void comment(){
+        System.out.println("test");
+        int num =1;
+
+        assertEquals(1,num);
+
+    }
+
+
+
+}

637c7d215a0317263425dd55a46e9ee79f675bb2
moon <moonlt93@naver.com>
2022-10-22 21:36:00 +0900

    commit 20

--- src/test/java/com/zerobase/luffy/member/common/controller/JoinControllerTest.java
+++ src/test/java/com/zerobase/luffy/member/common/controller/JoinControllerTest.java
@@ -1,15 +1,12 @@
 package com.zerobase.luffy.member.common.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@Transactional
 @WebAppConfiguration
 class JoinControllerTest {
 
@@ -17,9 +14,9 @@ class JoinControllerTest {
     @Test
     public void comment(){
         System.out.println("test");
-        int num =1;
+        int num =2;
 
-        assertEquals(1,num);
+        assertEquals(2,num);
 
     }
 
